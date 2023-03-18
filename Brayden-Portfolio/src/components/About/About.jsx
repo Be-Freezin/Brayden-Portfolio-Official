607440a8b31ced2b269d00b146ff9cda140b861d
Be-Freezin <brayf@live.com>
2023-03-18 10:50:46 -0500

    Build

--- Brayden-Portfolio/src/components/About/About.jsx
+++ Brayden-Portfolio/src/components/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
       <h2 className="mx-auto font-handcraftChalk text-5xl text-skin-accent lg:mb-20 lg:text-8xl">
         Get to know me
       </h2>
-      <div className="mx-auto mt-16 lg:flex lg:w-5/6 lg:flex-row lg:items-center lg:justify-evenly">
+      <div className="mx-auto mt-16 lg:flex lg:w-5/6 lg:flex-row lg:items-center lg:justify-evenly ">
         <Aboutbio />
         <div className="mx-auto  grid w-96  auto-cols-auto auto-rows-auto grid-cols-3  gap-y-5  gap-x-16 p-6">
           {interestsElements}
