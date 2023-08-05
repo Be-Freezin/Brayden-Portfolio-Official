587a9ab1cd2a50cd01125418a6f397c1d94089f7
Be-Freezin <brayf@live.com>
2023-08-04 19:44:38 -0500

    imgs to footer

--- Brayden-Portfolio/src/components/Footer/Footer.jsx
+++ Brayden-Portfolio/src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import scytheLeft from '../../../public/scythe left.svg'
+import scytheRight from '../../../public/scythe right.svg'
 
 const Footer = () => {
-  return (
+	return (
 		<div className=' flex justify-center border-t-2 border-bordermain py-6'>
-			<p className='font-paragraph text-skin-base '>
-				
-				Crafted with care and caffeine
-			</p>
+			<div className='flex flex-row items-center justify-evenly w-1/4'>
+				<img src={scytheLeft} alt='' srcset='' className='w-20' />
+				<p className='font-paragraph text-skin-base font-light '>
+					Crafted with care and caffeine
+				</p>
+				<img src={scytheRight} alt='' srcset='' className='w-20' />
+			</div>
 		</div>
 	)
 }
